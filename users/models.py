227bbdd23206ade908b07f004c4c96f1a2987490
Cameron Bowers <cameronbowers0@gmail.com>
2023-07-08 15:11:36 -0700

    Edit profile model to be a 1:1 to shelter. Add post_save to user to create profiles.

--- users/models.py
+++ users/models.py
@@ -6,7 +6,14 @@ from shelters.models import Shelter
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    employer = models.ForeignKey(Shelter, on_delete=models.SET_NULL, blank=True, null=True)
+    shelter = models.OneToOneField(Shelter, on_delete=models.SET_NULL, blank=True, null=True, default=None)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    @property
+    def is_shelter(self):
+        if self.shelter is None:
+            return False
+        else:
+            return True
