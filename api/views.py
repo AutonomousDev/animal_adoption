bc73785f4c6068015ca57552750a937b9eb64869
jay-lee-code <8713509+jay-lee-code@users.noreply.github.com>
2023-07-15 20:15:15 -0700

    Switch to class based view, add filter options

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
 from django.apps import apps
 from .serializers import AnimalSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+class AnimalFilterView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        queryset = apps.get_model("animals", "Animal").objects.all()
+        serializer = AnimalSerializer(queryset, many=True)
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def animal_list(req):
-    animals = apps.get_model("animals", "Animal").objects.all()
-    serialized = AnimalSerializer(animals, many=True)
-    print(serialized)
-    return Response(serialized.data)
+        return Response(serializer.data, status=200)
+
+    def post(self, request):
+        data = request.data
+        queryset = apps.get_model("animals", "Animal").objects.filter(**data)
+        serializer = AnimalSerializer(queryset, many=True)
+        
+        return Response(serializer.data, status=200)
