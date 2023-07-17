4fd0bc857f2235859b5e023c96e41a815898ea10
jay-lee-code <8713509+jay-lee-code@users.noreply.github.com>
2023-07-17 11:44:39 -0700

    Added read news, temporarily disabled auth

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
-from .views import AnimalFilterView
+from .views import AnimalFilterView, NewsListView
 
 urlpatterns = [
     path('animals/', AnimalFilterView.as_view()),
+    path('news/', NewsListView.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # This is basically the logout:
--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from django.apps import apps
-from .serializers import AnimalSerializer
+from .serializers import AnimalSerializer, NewsSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 
 class AnimalFilterView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     def get(self, request):
         queryset = apps.get_model("animals", "Animal").objects.all()
         serializer = AnimalSerializer(queryset, many=True)
@@ -18,3 +20,14 @@ class AnimalFilterView(APIView):
         serializer = AnimalSerializer(queryset, many=True)
         
         return Response(serializer.data, status=200)
+
+class NewsListView(ListAPIView):
+    # permission_classes = [IsAuthenticated]
+    queryset = apps.get_model("news", "News").objects.all()
+    serializer_class = NewsSerializer
+    pagination_class = LimitOffsetPagination
+
+    # Examples:
+    # 127.0.0.1:8000/api/news/
+    # 127.0.0.1:8000/api/news/?limit=1&offset=6
+    # 127.0.0.1:8000/api/news/?limit=3
