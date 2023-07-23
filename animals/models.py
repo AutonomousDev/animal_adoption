9a3ad854a1d51b7f1290753ec27cb32404f55cf5
Cameron Bowers <cameronbowers0@gmail.com>
2023-07-23 14:00:38 -0700

    Clean up

--- animals/models.py
+++ animals/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from shelters.models import Shelter
-from PIL import Image
 
 
 # Create your models here.
@@ -27,15 +26,6 @@ class Animal(models.Model):
         """Returns the url to access a detailed record for this animal"""
         return reverse('animals-detail', args=[str(self.id)])
 
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #
-    #     img = Image.open(self.image.path)
-    #
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
 
     class Meta:
         ordering = ['date_entered']
@@ -80,4 +70,3 @@ class Disposition(models.Model):
 
     def __str__(self):
         return self.disposition
-
