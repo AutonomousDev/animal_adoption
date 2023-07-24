4537a28a1fc2887ac7ff98648a5c3efd3bb111c5
jay-lee-code <8713509+jay-lee-code@users.noreply.github.com>
2023-07-23 23:35:37 -0700

    Added news list screen

--- ionic-adoption/src/pages/NewsList.tsx
+++ ionic-adoption/src/pages/NewsList.tsx
@@ -15,26 +15,62 @@ import {
     // IonPage,
     // IonTitle,
     // IonToolbar,
+    // useIonViewWillEnter,
 } from "@ionic/react";
 import React from "react";
+import { BASE } from "../constants";
 
 const NewsList: React.FC = () => {
+    // Declare a state variable to store the posts data
+    const [posts, setPosts] = React.useState([]);
+
+    // Define a function to fetch the posts data
+    async function getNews() {
+        const token = localStorage.getItem("token");
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        };
+
+        const response = await fetch(`${BASE}/news/`, requestOptions);
+
+        const data = await response.json();
+
+        setPosts(data);
+    }
+
+    React.useEffect(() => {
+        getNews();
+    }, []);
+
+    // useIonViewWillEnter(() => {
+    //     console.log("ionViewWillEnter event fired");
+    // });
+
     return (
         <IonContent>
-            <IonCard>
-                <IonCardHeader>
-                    <IonCardTitle>New Cat</IonCardTitle>
-                    <IonCardSubtitle>Placeholder cat</IonCardSubtitle>
-                </IonCardHeader>
+            {posts.map((post) => (
+                <IonCard>
+                    <IonCardHeader>
+                        <IonCardTitle>{post.title}</IonCardTitle>
+                        {/* <IonCardSubtitle></IonCardSubtitle> */}
+                    </IonCardHeader>
 
-                <IonCardContent>
-                    <img
-                        alt="Silhouette of mountains"
-                        src="https://placekitten.com/100/100"
-                    />
-                </IonCardContent>
-                <IonCardContent>A cat.</IonCardContent>
-            </IonCard>
+                    <IonCardContent>
+                        {/* <img
+                            alt="Silhouette of mountains"
+                            src="https://placekitten.com/100/100"
+                        /> */}
+                    </IonCardContent>
+                    <IonCardContent>
+                        <p>{post.author ? `By: ${post.author}` : ""}</p>
+                        <p>{post.body}</p>
+                    </IonCardContent>
+                </IonCard>
+            ))}
         </IonContent>
     );
 };
